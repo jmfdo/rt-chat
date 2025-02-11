b563d6ca2d8754969dfb5f3e272d083df4ba8209
Jose Fajardo <jmfajardorst@gmail.com>
2025-02-10 22:01:47 -0500

    feat: Create auth template

---
+++ server/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = (request, response) => {
+  response.send('Signup route')
+}
+
+export const login = (request, response) => {
+  response.send('Login route')
+}
+
+export const logout = (request, response) => {
+  response.send('Logout route')
+}
