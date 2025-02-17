0fa235a997ddadbb0bf1b3cdd0e116c376a17d00
Jose Fajardo <jmfajardorst@gmail.com>
2025-02-16 21:53:13 -0500

    feat: Express middleware and database connection

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,13 @@
 import express from 'express'
 import authRoutes from './routes/auth.route.js'
+import { connectDatabase } from './lib/db.js'
 
 const app = express()
+app.use(express.json())
 
 app.use('/api/auth', authRoutes)
 
 app.listen(5001, () => {
   console.log('Server is running on port 5001')
+  connectDatabase()
 })
